0f3d8a3e7e846059a6ca63f5c8ce979ef5ffe098
ShadiJam <shadijam00@gmail.com>
2017-01-08 17:09:03 -0600

    styling complete

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController
-before_action :set_place, only: [:show, :edit, :update, :destroy]
+before_action :set_place, only: [:show, :edit, :update, :destroy, :search]
   def index
     @places = Place.order('created_at DESC')
   end
